dbedbc95a276d8cf3cec6f588638a671bfe6fcec
Luke <luke@luke.co.pl>
2016-12-22 15:45:06 +0100

    Add ability to create random data points and centroids, and manually add centroids

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 let canvas = document.getElementsByTagName('canvas')[0],
     ctx = canvas.getContext('2d');
 
-let dataPoints = [];
+let dataPoints = [],
+    centroids = [];
 
-let randInt = function(min, max) {
-    if (arguments.length == 1) {
-        max = arguments[0];
-        min = 0;
-    }
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-let drawPoint = function([x, y]) {
+let drawDataPoint = function([x, y]) {
     ctx.beginPath();
     ctx.arc(x, y, 5, 0, 2 * Math.PI);
     ctx.fill();
 }
 
-let drawPoints = () => {
+let drawCentroid = function([x, y]) {
+    ctx.save()
+        ctx.beginPath();
+        ctx.arc(x, y, 8, 0, 2 * Math.PI);
+        ctx.stroke();
+        ctx.beginPath();
+        ctx.arc(x, y, 4, 0, 2 * Math.PI);
+        ctx.fill();
+    ctx.restore();
+}
+
+let redrawAll = function() {
     canvas.width = canvas.width;
-    for (let point of dataPoints) {
-        drawPoint(point);
-    }
+    dataPoints.map(drawDataPoint);
+    centroids.map(drawCentroid);
 }
 
 let addingDataPointsManually = false,
@@ -74,9 +77,50 @@ let pointClickedOnCanvas = function(e) {
 };
 
 canvas.addEventListener('click', function(e) {
+    let newPoint = pointClickedOnCanvas(e);
     if (addingDataPointsManually) {
-        let newPoint = pointClickedOnCanvas(e);
         dataPoints.push(newPoint)
-        drawPoint(newPoint);
+        drawDataPoint(newPoint);
+    } else if (addingCentroidsManually) {
+        centroids.push(newPoint)
+        drawCentroid(newPoint);
+    }
+}, false);
+
+let randInt = function(min, max) {
+    if (arguments.length == 1) {
+        max = arguments[0];
+        min = 0;
+    }
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+document.getElementById('add-data-points-randomly').addEventListener('click', function() {
+    let count = +document.getElementById('add-data-points-randomly-count').value;
+    for (let i = 0; i < count; ++i) {
+        let newPoint;
+        do {
+            newPoint = [
+                randInt(0, canvas.width - 1),
+                randInt(0, canvas.height - 1)
+            ];
+        } while (newPoint in centroids);
+        dataPoints.push(newPoint);
+        drawDataPoint(newPoint);
+    }
+}, false);
+
+document.getElementById('add-centroids-randomly').addEventListener('click', function() {
+    let count = +document.getElementById('add-centroids-randomly-count').value;
+    for (let i = 0; i < count; ++i) {
+        let newPoint;
+        do {
+            newPoint = [
+                randInt(0, canvas.width - 1),
+                randInt(0, canvas.height - 1)
+            ];
+        } while (newPoint in centroids);
+        centroids.push(newPoint);
+        drawCentroid(newPoint);
     }
 }, false);
